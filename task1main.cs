7d3a9a10c39ba6fff4c376e5e099e7fbc7bd6a8f
kekkill <137723801+kekkill@users.noreply.github.com>
2025-10-24 20:26:00 +0300

    fixed project

---
+++ task1main.cs
@@ -0,0 +1,12 @@
+﻿/*class task1main
+{
+    static void Main()
+    {
+        Game game = new Game();
+        game.StartGame();
+
+        Console.WriteLine("\n\nНажмите любую клавишу для выхода...");
+        Console.ReadKey();
+    }
+}
+*/
